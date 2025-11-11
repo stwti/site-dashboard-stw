45635bdbe82ce0ac7b546e75145eb4383932886a
Douglas Cardoso dos Santos <Dsantos@STUDIOW.COM.BR>
2025-11-11 13:04:13 -0300

    Adição de Usuário

--- dashboards.py
+++ dashboards.py
@@ -1,9 +1,4 @@
 todos_dashboards = {
-    "Campanha_Loreal_Coloração": {
-        "nome": "Campanha Loreal - Coloração",
-        "icone": "dashboard4.png",
-        "embed": "https://app.powerbi.com/view?r=eyJrIjoiNWZjYTdiZjAtN2FmZC00N2Q1LWJkMGMtNWY1NjdjMGYzYjYzIiwidCI6ImQ2MzMwOTY2LWY4NWItNGY0MS04NTFkLWE4OGZjOTNlOGM4YiJ9"
-    },
     "Vendas_Agosto_Reboucas": {
         "nome": "Vendas Agosto - Rebouças",
         "icone": "dashboard1.png",
@@ -19,97 +14,27 @@ todos_dashboards = {
         "icone": "dashboard1.png",
         "embed": "https://app.powerbi.com/view?r=eyJrIjoiOGQ5OGM5ZjItOTdmOC00Mjc0LWE0YmMtMzUzYmE0Mzg3ODZlIiwidCI6ImQ2MzMwOTY2LWY4NWItNGY0MS04NTFkLWE4OGZjOTNlOGM4YiJ9"
     },
-    "Wella_Assistentes": {
-        "nome": "Wella - Assistentes",
-        "icone": "dashboard3.png",
-        "embed": "https://app.powerbi.com/view?r=eyJrIjoiM2QwZjFhYjEtYjg1NC00ZGFhLTgzYjEtNGU4Y2Q5YmZiOGE3IiwidCI6ImQ2MzMwOTY2LWY4NWItNGY0MS04NTFkLWE4OGZjOTNlOGM4YiJ9"
-    },
-    "Wella_Rede": {
-        "nome": "Wella - Rede",
-        "icone": "dashboard2.png",
-        "embed": "https://app.powerbi.com/view?r=eyJrIjoiZWIzMGMxMDQtYWI4ZS00NTAwLTlhNmItNjY3OWI0MjAxNzFlIiwidCI6ImQ2MzMwOTY2LWY4NWItNGY0MS04NTFkLWE4OGZjOTNlOGM4YiJ9"
-    },
-    "Loreal_Rede": {
-        "nome": "Loreal - Rede",
-        "icone": "dashboard4.png",
-        "embed": "https://app.powerbi.com/view?r=eyJrIjoiMjQ0M2Y5YWYtYTE4NC00NDk3LWIxNTAtMjc5NjhiMzBlNGIyIiwidCI6ImQ2MzMwOTY2LWY4NWItNGY0MS04NTFkLWE4OGZjOTNlOGM4YiJ9"
-    },
-    "Wella_Gerentes_Rebouças": {
-        "nome": "Wella Gerentes - Rebouças",
-        "icone": "dashboard1.png",
-        "embed": "https://app.powerbi.com/view?r=eyJrIjoiYjVkMDVhYWMtNTc4Zi00NmM5LWJhYWItODg4ZWMxOGE4ZGM0IiwidCI6ImQ2MzMwOTY2LWY4NWItNGY0MS04NTFkLWE4OGZjOTNlOGM4YiJ9"
-    },
-    "Wella_Gerentes_Campinas": {
-        "nome": "Wella Gerentes - Campinas",
-        "icone": "dashboard4.png",
-        "embed": "https://app.powerbi.com/view?r=eyJrIjoiNDQ0MzhlMGQtYTQxMS00YWVjLTkxNTUtOGQ4ZjJiZDVmYTJjIiwidCI6ImQ2MzMwOTY2LWY4NWItNGY0MS04NTFkLWE4OGZjOTNlOGM4YiJ9"
-    },
-    "Wella_Gerentes_Higienopolis": {
-        "nome": "Wella Gerentes - Higienópolis",
-        "icone": "dashboard2.png",
-        "embed": "https://app.powerbi.com/view?r=eyJrIjoiMGM4ZjhmY2YtODFjNi00NjdjLWE4NjQtOTIxNTU3M2RhOTBjIiwidCI6ImQ2MzMwOTY2LWY4NWItNGY0MS04NTFkLWE4OGZjOTNlOGM4YiJ9"
-    },
-    "Wella_Gerentes_JK": {
-        "nome": "Wella Gerentes - JK",
-        "icone": "dashboard1.png",
-        "embed": "https://app.powerbi.com/view?r=eyJrIjoiMTY3YjI0MjEtZWYyNy00NGFhLWJjNTMtMzJkNTUwMzM4ZmJkIiwidCI6ImQ2MzMwOTY2LWY4NWItNGY0MS04NTFkLWE4OGZjOTNlOGM4YiJ9"
-    },
-    "Wella_Gerentes_Ribeirao": {
-        "nome": "Wella Gerentes - Ribeirão",
-        "icone": "dashboard4.png",
-        "embed": "https://app.powerbi.com/view?r=eyJrIjoiNDBlOWYyYjMtNWM4NS00OGM5LThmOTEtYTNkYjQ5NWEyMWEyIiwidCI6ImQ2MzMwOTY2LWY4NWItNGY0MS04NTFkLWE4OGZjOTNlOGM4YiJ9"
-    },
-    
-    "Loreal_Gerentes_Rebouças": {
-        "nome": "Loreal Gerentes - Rebouças",
-        "icone": "dashboard2.png",
-        "embed": "https://app.powerbi.com/view?r=eyJrIjoiYjU0YmE1YjctYWE5ZC00ZjdmLWJjYTAtOThmZjYwZmVlMTgyIiwidCI6ImQ2MzMwOTY2LWY4NWItNGY0MS04NTFkLWE4OGZjOTNlOGM4YiJ9"
-    },
-    
-    "Loreal_Gerentes_Campinas": {
-        "nome": "Loreal Gerentes - Campinas",
-        "icone": "dashboard1.png",
-        "embed": "https://app.powerbi.com/view?r=eyJrIjoiYzMwYTMzZWUtNmUzNy00ZTRmLWEzNDEtNDZmNzRhYTVlOGYwIiwidCI6ImQ2MzMwOTY2LWY4NWItNGY0MS04NTFkLWE4OGZjOTNlOGM4YiJ9"
-    },
-    
-    "Loreal_Gerentes_Higienopolis": {
-        "nome": "Loreal Gerentes - Higienópolis",
-        "icone": "dashboard3.png",
-        "embed": "https://app.powerbi.com/view?r=eyJrIjoiNmFmNzFhODQtMTRiMC00ZTk1LWJjYWEtMWNiZjhkOTI1MzFjIiwidCI6ImQ2MzMwOTY2LWY4NWItNGY0MS04NTFkLWE4OGZjOTNlOGM4YiJ9"
-    },
-    
-    "Loreal_Gerentes_JK": {
-        "nome": "Loreal Gerentes - JK",
-        "icone": "dashboard2.png",
-        "embed": "https://app.powerbi.com/view?r=eyJrIjoiOTFkMTZjYzItNjE1OC00YmMyLTg4MzMtODU1MzhjOGEwYjc0IiwidCI6ImQ2MzMwOTY2LWY4NWItNGY0MS04NTFkLWE4OGZjOTNlOGM4YiJ9"
-    },
-    
-    "Loreal_Gerentes_Ribeirao": {
-        "nome": "Loreal Gerentes - Ribeirão",
-        "icone": "dashboard4.png",
-        "embed": "https://app.powerbi.com/view?r=eyJrIjoiMmUxOTMyNzYtZjFkZS00NDEzLTlhM2QtMmY5ZTJlN2RmMjBhIiwidCI6ImQ2MzMwOTY2LWY4NWItNGY0MS04NTFkLWE4OGZjOTNlOGM4YiJ9"
-    },
-    "Vendas_Campinas": {
+    "Vendas_Novembro_Campinas": {
         "nome": "Vendas Novembro - Campinas",
         "icone": "dashboard2.png",
         "embed": "https://app.powerbi.com/view?r=eyJrIjoiYTdmYzg5MDUtYjBiMS00ZTUxLWE4ZjctNWEzMGNkMTEyYzNkIiwidCI6ImQ2MzMwOTY2LWY4NWItNGY0MS04NTFkLWE4OGZjOTNlOGM4YiJ9"
     },
-    "Vendas_Ribeirao": {
+    "Vendas_Novembro_Ribeirao": {
         "nome": "Vendas Novembro - Ribeirão Preto",
         "icone": "dashboard1.png",
         "embed": "https://app.powerbi.com/view?r=eyJrIjoiMDRhOTVjY2YtOWQwOC00OGM2LWI1ZGYtMGYwMWZhMjM5MWMxIiwidCI6ImQ2MzMwOTY2LWY4NWItNGY0MS04NTFkLWE4OGZjOTNlOGM4YiJ9"
     },
-    "Vendas_Reboucas": {
+    "Vendas_Novembro_Reboucas": {
         "nome": "Vendas Novembro - Rebouças",
         "icone": "dashboard2.png",
         "embed": "https://app.powerbi.com/view?r=eyJrIjoiNzRlNTEzY2MtZGVlNS00NGIxLWEyOGYtNDE3MWQwM2IyZDU1IiwidCI6ImQ2MzMwOTY2LWY4NWItNGY0MS04NTFkLWE4OGZjOTNlOGM4YiJ9"
     },
-    "Vendas_Higienopolis": {
+    "Vendas_Novembro_Higienopolis": {
         "nome": "Vendas Novembro - Higienópolis",
         "icone": "dashboard4.png",
         "embed": "https://app.powerbi.com/view?r=eyJrIjoiZmQ1YjU1M2ItMTY0OS00OTE3LThhM2ItYjcyODY0YzgzNjliIiwidCI6ImQ2MzMwOTY2LWY4NWItNGY0MS04NTFkLWE4OGZjOTNlOGM4YiJ9"
     },
-    "Vendas_JK": {
+    "Vendas_Novembro_JK": {
         "nome": "Vendas Novembro - JK",
         "icone": "dashboard3.png",
         "embed": "https://app.powerbi.com/view?r=eyJrIjoiMTYwNmFiZDEtZDgzNi00ZDAyLWJiZjYtMTllYzdkYjFiOWEyIiwidCI6ImQ2MzMwOTY2LWY4NWItNGY0MS04NTFkLWE4OGZjOTNlOGM4YiJ9"
--- usuarios.py
+++ usuarios.py
@@ -26,81 +26,52 @@ usuarios = {
             "Wella_Gerentes_Ribeirao",
             "Loreal_Gerentes_Ribeirao",
             "Wella_Gerentes_Ritu",
-            "Loreal_Gerentes_Ritu",
+            "Loreal_Gerentes_Ritu"
         ]
     },
     "greboucas": {
         "nome": "Gerência Rebouças",
         "senha": "PwbiRb@1808",
         "acesso": [
-            "Vendas_Reboucas",
-            "Wella_Assistentes",
-            "Wella_Rede",
-            "Campanha_Loreal_Coloração",
-            "Loreal_Rede",
-            "Wella_Gerentes_Rebouças",
-            "Loreal_Gerentes_Rebouças"
+            "Vendas_Novembro_Reboucas",
+            "Vendas_Agosto_Reboucas",
+            "Vendas_Setembro_Reboucas",
+            "Vendas_Julho_Reboucas"
         ]
     },
     "ghigi": {
         "nome": "Gerência Higienópolis",
         "senha": "PwbiHg@1809",
         "acesso": [
-            "Vendas_Higienopolis",
-            "Wella_Assistentes",
-            "Wella_Rede",
-            "Campanha_Loreal_Coloração",
-            "Loreal_Rede",
-            "Wella_Gerentes_Higienopolis",
-            "Loreal_Gerentes_Higienopolis"
+            "Vendas_Novembro_Higienopolis"
         ]
     },
     "gjk": {
         "nome": "Gerência JK",
         "senha": "PwbiJk@1811",
         "acesso": [
-            "Vendas_JK",
-            "Wella_Assistentes",
-            "Wella_Rede",
-            "Campanha_Loreal_Coloração",
-            "Loreal_Rede",
-            "Wella_Gerentes_JK",
-            "Loreal_Gerentes_JK"
+            "Vendas_Novembro_JK"
         ]
     },
     "gritu": {
         "nome": "Gerência Ritu",
         "senha": "PwbiRitu@1812",
         "acesso": [
-            "Wella_Assistentes",
-            "Wella_Rede",
-            "Campanha_Loreal_Coloração",
-            "Loreal_Rede"
+
         ]
     },
     "gcampinas": {
         "nome": "Gerência Campinas",
         "senha": "PwbiCamp@1813",
         "acesso": [
-            "Wella_Rede",
-            "Campanha_Loreal_Coloração",
-            "Loreal_Rede",
-            "Wella_Gerentes_Campinas",
-            "Loreal_Gerentes_Campinas",
-            "Vendas_Campinas"
+            "Vendas_Novembro_Campinas"
         ]
     },
     "gribeirao": {
         "nome": "Gerência Ribeirão",
         "senha": "PwbiRib@1813",
         "acesso": [
-            "Vendas_Ribeirao",
-            "Wella_Assistentes",
-            "Wella_Rede",
-            "Campanha_Loreal_Coloração",
-            "Loreal_Rede",
-            "Wella_Gerentes_Ribeirao",
-            "Loreal_Gerentes_Ribeirao"
+            "Vendas_Novembro_Ribeirao"
            
         ]
     },
@@ -108,108 +79,44 @@ usuarios = {
         "nome": "Herbert Navarini",
         "senha": "Hnavarini@1815",
         "acesso": [
-            "Vendas_Campinas",
-            "Vendas_Ribeirao",
-            "Vendas_Reboucas",
-            "Vendas_Higienopolis",
-            "Vendas_JK",
-            "Wella_Assistentes",
-            "Wella_Rede",
-            "Campanha_Loreal_Coloração",
-            "Loreal_Rede",
-            "Wella_Gerentes_Rebouças",
-            "Loreal_Gerentes_Rebouças",
-            "Wella_Gerentes_Campinas",
-            "Loreal_Gerentes_Campinas",
-            "Wella_Gerentes_Higienopolis",
-            "Loreal_Gerentes_Higienopolis",
-            "Wella_Gerentes_JK",
-            "Loreal_Gerentes_JK",
-            "Wella_Gerentes_Ribeirao",
-            "Loreal_Gerentes_Ribeirao",
-            "Wella_Gerentes_Ritu",
-            "Loreal_Gerentes_Ritu",
+            "Vendas_Novembro_Campinas",
+            "Vendas_Novembro_Ribeirao",
+            "Vendas_Novembro_Reboucas",
+            "Vendas_Novembro_Higienopolis",
+            "Vendas_Novembro_JK"
         ]
     },
     "comercialw": {
         "nome": "Comercial Studio W",
         "senha": "@!Comercial2108",
         "acesso": [
-            "Vendas_Campinas",
-            "Vendas_Ribeirao",
-            "Vendas_Reboucas",
-            "Vendas_Higienopolis",
-            "Vendas_JK",
-            "Wella_Assistentes",
-            "Wella_Rede",
-            "Campanha_Loreal_Coloração",
-            "Loreal_Rede",
-            "Wella_Gerentes_Rebouças",
-            "Loreal_Gerentes_Rebouças",
-            "Wella_Gerentes_Campinas",
-            "Loreal_Gerentes_Campinas",
-            "Wella_Gerentes_Higienopolis",
-            "Loreal_Gerentes_Higienopolis",
-            "Wella_Gerentes_JK",
-            "Loreal_Gerentes_JK",
-            "Wella_Gerentes_Ribeirao",
-            "Loreal_Gerentes_Ribeirao",
-            "Wella_Gerentes_Ritu",
-            "Loreal_Gerentes_Ritu",
+            "Vendas_Novembro_Campinas",
+            "Vendas_Novembro_Ribeirao",
+            "Vendas_Novembro_Reboucas",
+            "Vendas_Novembro_Higienopolis",
+            "Vendas_Novembro_JK"
         ]
     },
     "financeirow": {
         "nome": "Financeiro Studio W",
         "senha": "@!Financeiro0821",
         "acesso": [
-            "Vendas_Campinas",
-            "Vendas_Ribeirao",
-            "Vendas_Reboucas",
-            "Vendas_Higienopolis",
-            "Vendas_JK",
-            "Wella_Assistentes",
-            "Wella_Rede",
-            "Campanha_Loreal_Coloração",
-            "Loreal_Rede",
-            "Wella_Gerentes_Rebouças",
-            "Loreal_Gerentes_Rebouças",
-            "Wella_Gerentes_Campinas",
-            "Loreal_Gerentes_Campinas",
-            "Wella_Gerentes_Higienopolis",
-            "Loreal_Gerentes_Higienopolis",
-            "Wella_Gerentes_JK",
-            "Loreal_Gerentes_JK",
-            "Wella_Gerentes_Ribeirao",
-            "Loreal_Gerentes_Ribeirao",
-            "Wella_Gerentes_Ritu",
-            "Loreal_Gerentes_Ritu",
+            "Vendas_Novembro_Campinas",
+            "Vendas_Novembro_Ribeirao",
+            "Vendas_Novembro_Reboucas",
+            "Vendas_Novembro_Higienopolis",
+            "Vendas_Novembro_JK"
         ]
     },
     "rosangelab": {
         "nome": "Rosângela Barchetta",
         "senha": "@!Diretoria1005",
         "acesso": [
-            "Vendas_Campinas",
-            "Vendas_Ribeirao",
-            "Vendas_Reboucas",
-            "Vendas_Higienopolis",
-            "Vendas_JK",
-            "Wella_Assistentes",
-            "Wella_Rede",
-            "Campanha_Loreal_Coloração",
-            "Loreal_Rede",
-            "Wella_Gerentes_Rebouças",
-            "Loreal_Gerentes_Rebouças",
-            "Wella_Gerentes_Campinas",
-            "Loreal_Gerentes_Campinas",
-            "Wella_Gerentes_Higienopolis",
-            "Loreal_Gerentes_Higienopolis",
-            "Wella_Gerentes_JK",
-            "Loreal_Gerentes_JK",
-            "Wella_Gerentes_Ribeirao",
-            "Loreal_Gerentes_Ribeirao",
-            "Wella_Gerentes_Ritu",
-            "Loreal_Gerentes_Ritu",
+            "Vendas_Novembro_Campinas",
+            "Vendas_Novembro_Ribeirao",
+            "Vendas_Novembro_Reboucas",
+            "Vendas_Novembro_Higienopolis",
+            "Vendas_Novembro_JK"
         ]
     }
 }
